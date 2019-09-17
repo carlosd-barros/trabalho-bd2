17b83766dfb9834614008267b7418cb9143fffe1
carlosd-barros <carlosd@rede.ulbra.br>
2019-09-17 12:00:10 -0300

    criando visualização dos dados da tabela Clientes

--- queryzitas/forms.py
+++ queryzitas/forms.py
@@ -11,24 +11,41 @@ class InsertForm(forms.Form):
     nome = forms.CharField(
         label='Nome',
         max_length=100,
-        required=False
+        required=True
     )
     cpf = forms.CharField(
         label='CPF',
         max_length=14,
-        required=False
+        required=True
     )
     cidade = forms.CharField(
         label='Cidade',
         max_length=50,
-        required=False
+        required=True
     )
 
 class DeleteForm(forms.Form):
     pass
 
 class ReadForm(forms.Form):
-    pass
+    cod = forms.IntegerField(
+        label='Código',
+        required=False
+    )
+    nome = forms.CharField(
+        label='Nome',
+        required=False
+    )
+
+    """def clean(self):
+        cleaned_data = super().clean()
+        cod = cleaned_data.get('cod')
+        nome = cleaned_data.get('nome')
+
+        if not cod and not nome:
+            raise forms.ValidationError(
+                "Preencha pelo menos um campo."
+            )"""
 
 class FaturamentoForm(forms.Form):
     pass
--- queryzitas/views.py
+++ queryzitas/views.py
@@ -26,7 +26,7 @@ class InsertFormView(SuccessMessageMixin, FormView):
     success_message = 'Dados inseridos com sucesso.'
     
     def form_valid(self, form):
-        dados = form.clean() 
+        dados = form.clean()
         if self.request.method == "POST":
             if form.is_valid():
                 with connection.cursor() as cursor:
@@ -52,13 +52,62 @@ class DeleteFormView(FormView):
     success_url = 'queryzitas:delete'
 
 
-class ReadFormView(FormView):
+class ReadFormView(SuccessMessageMixin, FormView):
+    resultados = None
     form_class = ReadForm
     template_name = 'Read.html'
-    success_url = reverse_lazy('queryzitas:read')
+    success_url = 'queryzitas:read'
+    success_message = 'Consulta realizada com sucesso.'
+
+    def form_valid(self, form):
+        dados = form.clean()
+        print(f"""
+            cod: {dados['cod']}, {type(dados['cod'])}
+            nome: {dados['nome']}, {type(dados['nome'])}
+        """)
+        if self.request.method == "POST":
+            if form.is_valid():
+                with connection.cursor() as cursor:
+                    if dados['cod']:
+                        cursor.execute(f"""
+                            exec [dbo].[read_cli] {dados['cod']};
+                        """)
+                        self.resultados = cursor.fetchall()
+                        print(f"""
+                            cursor: {cursor.fetchall()}
+                        """)
+                    else:
+                        cursor.execute(f"""
+                            select * from Cliente;
+                        """)
+                        print(f"""
+                            cursor: {cursor.fetchall()}
+                        """)
+                    self.resultados = cursor.fetchall()
+            else:
+                form = InsertForm()
+
+        return super(
+            ReadFormView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        print('entrou no get context', self.resultados)
+        kwargs.update({
+            'resultados': self.resultados
+        })
+
+        return super(
+            ReadFormView, self).get_context_data(**kwargs)
+
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message
+
+    def get_success_url(self):
+        return reverse(self.success_url)
 
 class FaturamentoFormView(FormView):
     form_class = FaturamentoForm
     template_name = 'Faturamento.html'
-    success_url = reverse_lazy('queryzitas:faturamneto')
+    success_url = 'queryzitas:faturamneto'
     
